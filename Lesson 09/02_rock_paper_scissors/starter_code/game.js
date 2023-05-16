fc4e2407898ad681c9137d9030c31f7e1e580cca
“bforino” <bdigiovine@parrylabs.com>
2023-05-16 03:23:45 -0500

    updates from class 9

---
+++ Lesson 09/02_rock_paper_scissors/starter_code/game.js
@@ -0,0 +1,68 @@
+
+
+
+
+// Create a function that determines the computer choice.  It should return one of the three values: 'rock', 'paper', or 'scissors'
+function computerChoice() {
+
+    //Create a random number (between 0 and 1)
+    //Multiply the number by 3 to make it between 1 and 3
+    //Round number up
+
+    // if 1, it's a rock
+
+    // if 2, it's a paper
+
+    // if 3, it's a scissors
+
+
+};
+
+
+/* Create a function that compares the user's selection to the computer's selection.  It should then output the opponent's choice to #computerSelection and display who is the winner.
+
+ */
+function compare(me, opponent) {
+
+    // Decide who wins
+
+    // If input is rock...
+
+    // Once the winner is decided, target decision or target the ID decision and display who wins.
+
+};
+
+
+
+document.addEventListener('DOMContentLoaded', function(event) {
+
+    // Bind a click event onto each of the three buttons.  It should run the compare function and then put the result to the #decsision id in the html
+
+    //When I click on scissors
+
+        //Prevent the link from executing default
+
+        //Get the computer's choice
+
+        //compare scissors to the computer choice
+
+
+    //When I click on paper
+
+        //Prevent the link from executing default
+
+        //Get the computer's choice
+
+        //compare paper to the computer choice
+
+    //When I click on rock
+
+        //Prevent the link from executing default
+
+        //Get the computer's choice
+
+        //compare rock to the computer choice
+
+
+
+});
