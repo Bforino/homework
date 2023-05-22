e893c6cf0364aea4f8088c6034880d0df81d665d
“bforino” <bdigiovine@parrylabs.com>
2023-05-22 17:04:56 -0500

    Homework 4

--- Homework 04/starter_code/level-three.js
+++ Homework 04/starter_code/level-three.js
@@ -7,3 +7,44 @@ let arenaEl = document.querySelector('.js-arena');
 let scoreEl = document.querySelector('.js-score');
 
 
+
+function addPoints20() {
+    score = score + 20;
+    if( score <= 100 ) {
+      scoreEl.innerText = score;
+    } else {
+      declareWinner();
+    }
+  }
+
+  function addPoints5(){
+    score = score + 5;
+    if (score <= 100 ) {
+        scoreEl.innerText = score;
+    } else {
+        declareWinner();
+        }
+    }
+
+    function addPoints10(){
+        score = score + 10;
+        if (score <= 100 ) {
+            scoreEl.innerText = score;
+        } else {
+            declareWinner();
+            }
+        }
+
+    function declareWinner(){
+        document.body.classList.add('game-over');
+        }
+
+
+
+document.querySelector(`.js-ball[data-increment="20"]`).addEventListener('click', addPoints20);
+
+document.querySelector(`.js-ball[data-increment="5"]`).addEventListener('click', addPoints5);
+
+document.querySelector(`.js-ball[data-increment="10"]`).addEventListener('click', addPoints10);
+
+
