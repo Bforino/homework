6303ef8635561e1abef9e97b6fc740aa632098dd
“bforino” <bdigiovine@parrylabs.com>
2023-06-06 15:45:40 -0500

    Homework4

---
+++ Homework 06/starter_code/js/trulia.js
@@ -0,0 +1,51 @@
+
+
+let truliaCards = document.getElementsByClassName("trulia-grid-item");
+
+
+
+document.addEventListener('DOMContentLoaded', function(event) {
+
+  
+  //get the toggle icon
+  const toggleIcon = document.querySelector('.trulia-nav-toggle');
+
+  //Add Event Listener for toggle icon
+
+  toggleIcon.addEventListener('click', toggleNav)
+
+//create function to toggle "trulia-nav-mobilehide"
+
+function toggleNav(){
+  document.nav.classList.toggle('trulia-nav-mobilehide');
+}
+
+
+
+//toggleIcon.addEventListener('click', sayHello)
+
+//function sayHello(){
+//  console.log('hello');
+
+//create function to toggle "trulia-nav-mobilehide"
+
+
+
+
+
+
+// Loop through all the cards
+    
+  // Add a click listener on each card
+      
+      // Remove the featured class
+
+
+      // Add the featured class on the one clicked on
+
+
+
+  });  
+
+
+
---
+++ Lesson 13/05_CitiPix/starter_code/citipix.js
@@ -0,0 +1,33 @@
+// Create an array for list of cities
+let cities = ["NYC", "SF", "LA", "ATX", "SYD"];
+
+
+// Listen for when DOM is loaded
+document.addEventListener('DOMContentLoaded', function(event) {
+// When DOM is loaded, add cities to the dropdown options via loop
+for (let i = 0; i < cities.length; i++){
+    //Create the element
+    let citiesItem = document.createElement("option");
+    //Set the innerText to the element
+    citiesItem.innerText = cities[i];
+    citiesItem.value = cities[i];
+    //append element to the DOM
+    document.querySelector("#city-type").append(citiesItem);
+}
+
+// When selected, change background of the page
+// Listen for when user selects an item from the dropdown
+document.querySelector("#city-type").addEventListener("change", function(event){
+    //Get the picked value
+    let items = document.querySelector("#city-type").value;
+    //If/else if statements to match the picked value
+    if(items == 'NYC') {
+        document.body.setAttribute
+    }
+})
+
+
+
+
+    
+});
